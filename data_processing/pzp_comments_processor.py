8e2c60a048625dcd8deaddf24d1e223c5e6fbcea
takado <kornelgut@gmail.com>
2024-02-07 14:58:29 +0100

    extract nb and remove empty lines

--- data_processing/pzp_comments_processor.py
+++ data_processing/pzp_comments_processor.py
@@ -2,6 +2,25 @@ import json
 import re
 
 
+def extract_article_number(string):
+    numbers = re.findall(r'\d+', string)
+    assert len(numbers) == 1
+    return numbers.pop()
+
+
+def remove_empty_lines(article):
+    result = []
+    empty_lines = []
+    for line in article:
+        if line.strip() != '':
+            result.append(line)
+        else:
+            empty_lines.append(line)
+    # make sure only empty lines were removed
+    assert ''.join(empty_lines).strip() == ''
+    return result
+
+
 def remove_header_type_2(lines):
     result_lines = []
     removed_lines = []
@@ -39,6 +58,7 @@ def extract_articles(filename):
             # new article starts
             if current_article:
                 current_article = remove_header_type_2(current_article)
+                current_article = remove_empty_lines(current_article)
                 articles.append(current_article)
             current_article = [line]
         elif articles_started:
@@ -59,11 +79,6 @@ def extract_articles(filename):
                     continue
                 else:
                     header_started = True
-                    # remove previous 2 empty lines
-                    for _ in range(2):
-                        if current_article[-1].strip() == '':
-                            empty1 = current_article.pop(-1)
-                            empty_lines.append(empty1)
 
             if not header_started:
                 current_article.append(line)
@@ -75,19 +90,36 @@ def extract_articles(filename):
     return articles
 
 
-def process_comments(filename):
+def process_comments(filename, output_filename):
     articles = extract_articles(filename)
+    datapoints = []
     for i, article in enumerate(articles, start=1):
-        # article_nb_line = article.pop(0)
-        # print(f'{i}: {article_nb_line.strip()}')
+        article_nb_line = article[0]
+        article_nb = extract_article_number(article_nb_line.strip())
+        # print(f'{i}: {article_nb}')
         article_string = ''.join(article)
-        print(f'{i}: {article_string}')
+        entry = {
+            'txt': article_string,
+            "metadata": {
+                "chapter": "0",
+                "paragraph": article_nb,
+                "title": "0",
+                "date": "20.05.2021",
+                "type": "kdnpzp"
+            }
+        }
+        datapoints.append(entry)
+        # print(f'{i}: {article_string}')
+    with open(output_filename, 'w+', encoding='utf-8') as f:
+        json.dump(datapoints, f)
+    print(f'{len(datapoints)} entries saved to file: {output_filename}')
 
 
 if __name__ == '__main__':
     filename = '../data/md/pzp_comments.md'
+    output_filename = '../data/json/pzp_comments.json'
 
-    process_comments(filename)
+    process_comments(filename, output_filename)
 
     #
     # for i, text in enumerate(extracted_articles[:20], start=1):
